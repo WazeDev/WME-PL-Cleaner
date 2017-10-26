a1974b27a3335bcf0953f3482c5d6462263b2149
justins83 <justins83.waze@gmail.com>
2017-10-26 10:41:45 -0400

    Update plcleaner.js

--- plcleaner.js
+++ plcleaner.js
@@ -1,7 +1,7 @@
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-         return {redirectUrl: details.url.replace(/&[^&]*Filter=[^&]*|&s=(\d+)/ig,'')};
+         return {redirectUrl: details.url.replace(/&[^&]*Filter=[^&]*|&s=(\d+)/ig,'').replace('editor?','editor/?')};
     },
 	{urls: ["*://www.waze.com/editor*", "*://www.waze.com/*-*/editor*", "*://beta.waze.com/editor*", "*://beta.waze.com/*-*/editor*"]},
     ["blocking"]
