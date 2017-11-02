2295a5c333df0c5583dc873dca15ecaacabd0072
justins83 <justins83.waze@gmail.com>
2017-11-02 14:00:51 -0400

    Add files via upload
    Firefox compatibility

--- plcleaner.js
+++ plcleaner.js
@@ -1,8 +1,10 @@
 
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-         return {redirectUrl: details.url.replace(/&[^&]*Filter=[^&]*|&s=(\d+)/ig,'').replace('editor?','editor/?')};
-    },
+function listen(requestDetails) {
+		if(/.*((editor\?)+|(&s=(\d+))+).*/.test(requestDetails.url))
+         return {redirectUrl: requestDetails.url.replace(/&[^&]*Filter=[^&]*|&s=(\d+)/ig,'').replace('editor?','editor/?')};
+}
+browser.webRequest.onBeforeRequest.addListener(
+    listen,
 	{urls: ["*://www.waze.com/editor*", "*://www.waze.com/*-*/editor*", "*://beta.waze.com/editor*", "*://beta.waze.com/*-*/editor*"]},
     ["blocking"]
 );
