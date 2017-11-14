aff3be12f67d48316b40feee25e495923ee60633
justins83 <justins83.waze@gmail.com>
2017-11-14 09:57:01 -0500

    Setting zoom to 2 if it is 0|1 and there are selected segments

--- plcleaner.js
+++ plcleaner.js
@@ -1,8 +1,14 @@
 
+function listen(details)
+{
+	var replaceURL = details.url.replace(/&[^&]*Filter=[^&]*|&s=(\d+)/ig,'').replace('editor?','editor/?');
+	if(replaceURL.indexOf("&segments") > -1)
+		replaceURL = details.url.replace(/&zoom=(0|1)/, "&zoom=2");
+	return {redirectUrl: replaceURL};
+}
+
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-         return {redirectUrl: details.url.replace(/&[^&]*Filter=[^&]*|&s=(\d+)/ig,'').replace('editor?','editor/?')};
-    },
+    listen,
 	{urls: ["*://www.waze.com/editor*", "*://www.waze.com/*-*/editor*", "*://beta.waze.com/editor*", "*://beta.waze.com/*-*/editor*"]},
     ["blocking"]
 );
